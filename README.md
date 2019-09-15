32686e80354c4e365aebdd62556a86ecce828678
Onur Kerimov <onurkerimov@gmail.com>
2019-09-16 00:38:10 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Foldmaker
+A small parser generator toolkit that can be used to build parsers, transpilers, formatters.
+## Example
+## API
+
+### Foldmaker class constructor
+### tokenize()
+### flatten()
+### traverse()
+
+## LICENSE [MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,129 @@
+// Main class
+class Foldmaker {
+  constructor(tokens) {
+    if (tokens && !tokens.array && !tokens.string) {
+      this.array = tokens.map(el => el.value);
+      this.string = tokens.map(el => el.type).join("");
+    } else {
+      this.array = [];
+      this.string = "";
+    }
+    this.modified = false;
+  }
+  parse(tokens, directives) {
+    let map = this;
+    let emptyMap = new Foldmaker();
+    do {
+      tokenize(map.string, tokens, direct(map.array, directives, emptyMap));
+      map = emptyMap;
+      emptyMap = new Foldmaker();
+    } while (map.modified === true);
+    return map.array;
+  }
+  // To be used on an empty foldmaker
+  add(string, object) {
+    this.array.push(object);
+    this.string += string;
+    this.modified = true;
+  }
+
+  noop(result) {
+    this.array = this.array.concat(result[0]);
+    this.string += result.m[0];
+  }
+}
+
+// Foldmaker constructor
+export let from = tokens => new Foldmaker(tokens);
+
+// Basic traversal
+export let traverse = (array, callback) => {
+  array.forEach(el => {
+    callback(el);
+  });
+};
+
+// Generally used for finalization
+export let flatten = (array, callback) => {
+  let accumulator = [];
+  let flattenInner = () => {
+    array.forEach(el => {
+      if (Array.isArray(el)) {
+        accumulator = accumulator.concat(flattenInner(el));
+      } else if (typeof el === "object") {
+        accumulator = accumulator.concat(callback(el));
+      } else {
+        accumulator = accumulator.concat(el);
+      }
+    });
+  };
+  flattenInner();
+  return accumulator.join("");
+};
+
+export let tokenize = (string, TOKENS, CALLBACK) => {
+  let index = 0;
+  let tokens = [];
+  let len = TOKENS.length;
+
+  while (string) {
+    for (let i = 0; i < len; i += 1) {
+      // Try to find a token. If not found, go to the next iteration of the loop
+      let m = TOKENS[i][1].exec(string);
+      if (!m || m.index !== 0) continue;
+      let { type, value } = {
+        type: TOKENS[i][0],
+        value: m[0]
+      };
+
+      tokens.push(
+        CALLBACK ? CALLBACK({ type, value, m, index }) : { type, value }
+      );
+
+      // Advance by slicing the string and push tokens to the list
+      string = string.slice(value.length);
+      index += value.length;
+      break;
+    }
+  }
+  return tokens;
+};
+
+export let getOccurences = (array, m, index) => {
+  let count = m[0].length;
+  let whole = array.slice(index, count + index);
+  let occurences = [whole];
+
+  let cursor = 0;
+  m.forEach((el, i) => {
+    if (i !== 0) {
+      if (el) {
+        let count = el.length;
+        let occ = whole.slice(cursor, count + cursor);
+        occurences.push(occ);
+        cursor = count + cursor;
+      } else {
+        occurences.push(null);
+      }
+    }
+  });
+  occurences.m = m;
+  occurences.index = index;
+  occurences.count = m[0].length;
+  return occurences;
+};
+
+export let direct = (array, directives, self) => ({
+  type,
+  value,
+  m,
+  index
+}) => {
+  if (directives[type]) {
+    let result = getOccurences(array, m, index);
+    let returnValue = directives[type](result);
+    if (returnValue) self.add(returnValue, "o");
+  } else {
+    self.noop(result);
+  }
+};
