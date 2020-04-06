fdeb0490455295da8d862c3cf51efd178b643187
Onur Kerimov <onurkerimov@gmail.com>
2020-04-06 16:09:17 +0300

    Update

--- index.js
+++ index.js
@@ -1,113 +1,115 @@
-class FoldmakerObject {
+export class FoldmakerObject {
   constructor(obj) {
     let hasTokens = obj && Array.isArray(obj)
-    this.array = hasTokens ? obj.map(el => el.value) : []
-    this.string = hasTokens ? obj.map(el => el.type).join('') : ''
+    this.array = hasTokens ? obj.map((el) => el.value) : []
+    this.string = hasTokens ? obj.map((el) => el.type).join('') : ''
     this.modified = false
+    // These are here: easy extendability
+    this.__proto__._tokenize = tokenize
+    this.__proto__._traverse = traverse
   }
 
-  replace(visitors, callback) {
-    let tokens = getTokensFromVisitors(visitors, callback)
-    return replace(this, tokens)
+  replace(...args) {
+    const { tokens, debug } = this._getDataFromArguments(args)
+    return this._replace(this, tokens, debug)
   }
 
-  parse(visitors, callback, debug) {
-    let tokens = getTokensFromVisitors(visitors, callback)
-    if (Array.isArray(visitors)) debug = callback
-    let self = this
+  parse(...args) {
+    const { tokens, debug } = this._getDataFromArguments(args)
+    const self = this
     do {
-      self = replace(self, tokens)
-      if(debug) debug(self)
+      self = this._replace(self, tokens, debug)
     } while (self.modified === true)
     return self
   }
-  
+
   traverse(callback) {
-    this.array = traverse(this.array, callback)
+    this.array = this._traverse(this.array, callback)
     return this
   }
 
-  call(callback) {
-    return callback(this)
-  }
-
-  concatenate(string, object) {
+  add(string, object) {
     this.array = this.array.concat(object)
     this.string += string
   }
+  
+  _getDataFromArguments([tokens, callback, debug]) {
+    if (tokens instanceof RegExp) {
+      tokens = [[callback, tokens]]
+    } else {
+      tokens = tokens.map(([a, b]) => [b, a])
+      debug = callback
+    }
+    // Add this as the last token by default, this will prevent infinite loops
+    tokens.push([() => undefined, /[\s\n\S]/])
+    return { tokens, debug }
+  }
+
+  _replace(oldState, tokens, debug) {
+    let state = Foldmaker()
+    let { string, array } = oldState
+    this._tokenize(string, tokens, ({ type, map, index }) => {
+      const occurrence = this._getOccurrence(array, map, index)
+      this._manipulate(type, occurrence, state, oldState)
+    })
+    if(debug) debug(state)
+    return state
+  }
+  
+  _getOccurrence (array, map, index) {
+    let count = map[0].length
+    return {
+      raw: array.slice(index, count + index),
+      index, count, map
+    }
+  }
+  
+  _manipulate (type, occurrence, state, oldState) {
+    const result = type(occurrence, state, oldState)
+    if (result) {
+      if (Array.isArray(result)) state.add(result[0], [result[1]])
+      else state.add('1', [result])
+      state.modified = true
+    } else {
+      state.add(occurrence.map[0], occurrence.raw)
+    }
+  }
 }
 
-export const traverse = (node, callback) => {
-  let also = subNode => subNode && traverse(subNode, callback)
-  if(Array.isArray(node)) return node.map((item) => callback(item, also))
-  else return callback(node, also)
-}
-
-export let tokenize = (STRING, TOKENS, CALLBACK) => {
+export const tokenize = (string, dictionary, callback) => {
   let index = 0, tokens = []
   // Add this as the last token by default, this will prevent infinite loops
-  TOKENS.push(['0', /[\s\n\S]/])
-
-  while (STRING) {
-    TOKENS.some(([type, regex]) => {
-      let map = regex.exec(STRING)
+  dictionary.push(['0', /[\s\n\S]/])
+  while (string) {
+    dictionary.some(([type, regex]) => {
+      let map = regex.exec(string)
       if (map && map.index === 0) {
         // If not found, we are here
-        let value = map[0]
-        let returnValue = CALLBACK ? CALLBACK({ type, value, map, index }) : { type, value }
-        if (returnValue) tokens.push(returnValue) // TODO:
-
+        const value = map[0]
+        const tokenValue = callback ? callback({ type, value, map, index }) : { type, value }
+        if (tokenValue) tokens.push(tokenValue)
         // Advance by slicing the string and push tokens to the list
-        STRING = STRING.slice(value.length)
+        string = string.slice(value.length)
         index += value.length
-        return true 
+        return true
       }
     })
   }
   return tokens
 }
 
-export const getTokensFromVisitors = (tokens, callback) => {
-  if (tokens instanceof RegExp) tokens = [[callback, tokens]]
-  else tokens = tokens.map(([a,b]) => [b,a])
-  // Add this as the last token by default, this will prevent infinite loops
-  tokens.push([() => undefined, /[\s\n\S]/])
-  return tokens
+export const traverse = (node, callback) => {
+  let also = (subNode) => subNode && traverse(subNode, callback)
+  if (Array.isArray(node)) return node.map((item) => callback(item, also))
+  else return callback(node, also)
 }
 
-export let visitor = (a, b, c) => ([a, b, c])
+export const visitor = (a, b) => [a, b]
 
-export const getOccurrence = (array, map, index) => {
-  let count = map[0].length
-  return {
-    raw: array.slice(index, count + index),
-    index, count, map
-  }
-}
-
-export const replace = (self, tokens) => {
-  let state = Foldmaker()
-  let { string, array } = self
-  tokenize(string, tokens, ({ type, map,index }) => {
-    const occurrence = getOccurrence(array, map, index)
-    manipulate(state, type, occurrence)
-  })
-  return state
-}
-
-export const manipulate = (state, type, occurrence) => {
-    const result = type(occurrence)
-    if (result) {
-      if (Array.isArray(result)) state.concatenate(result[0], [result[1]])
-      else state.concatenate('1', [result])
-      state.modified = true
-    } else {
-      state.concatenate(occurrence.map[0], occurrence.raw)
-    }
-}
-
-const Foldmaker = tokens => new FoldmakerObject(tokens)
+const Foldmaker = (tokens) => new FoldmakerObject(tokens)
+Foldmaker.FoldmakerObject = FoldmakerObject
 Foldmaker.traverse = traverse
 Foldmaker.tokenize = tokenize
 Foldmaker.visitor = visitor
+Foldmaker.fm = Foldmaker // shorthand, circular
 export default Foldmaker
