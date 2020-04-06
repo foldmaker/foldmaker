728ebea008e8cdd61ff18136f25ca40287a26d5e
Onur Kerimov <onurkerimov@gmail.com>
2020-04-06 11:56:54 +0300

    Refactor

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 class FoldmakerObject {
   constructor(obj) {
-    if (obj && typeof Array.isArray(obj)) {
-      this.array = obj.map(el => el.value)
-      this.string = obj.map(el => el.type).join('')
-    } else {
-      this.array = []
-      this.string = ''
-    }
+    let hasTokens = obj && Array.isArray(obj)
+    this.array = hasTokens ? obj.map(el => el.value) : []
+    this.string = hasTokens ? obj.map(el => el.type).join('') : ''
     this.modified = false
   }
 
@@ -15,11 +11,13 @@ class FoldmakerObject {
     return replace(this, tokens)
   }
 
-  parse(visitors, callback) {
+  parse(visitors, callback, debug) {
     let tokens = getTokensFromVisitors(visitors, callback)
+    if (Array.isArray(visitors)) debug = callback
     let self = this
     do {
       self = replace(self, tokens)
+      if(debug) debug(self)
     } while (self.modified === true)
     return self
   }
@@ -33,20 +31,11 @@ class FoldmakerObject {
     return callback(this)
   }
 
-  // To be used on an empty foldmaker
-  add(string, object) {
-    this.array.push(object)
+  concatenate(string, object) {
+    this.array = this.array.concat(object)
     this.string += string
-    this.modified = true
-  }
-
-  noop(result) {
-    this.array = this.array.concat(result[0]) // TODO
-    this.string += result.map[0]
   }
 }
-
-export let visitor = (token, directive) => ([directive, token])
 
 export const traverse = (node, callback) => {
   let also = subNode => subNode && traverse(subNode, callback)
@@ -55,75 +44,66 @@ export const traverse = (node, callback) => {
 }
 
 export let tokenize = (STRING, TOKENS, CALLBACK) => {
-  let index = 0
-  let tokens = []
+  let index = 0, tokens = []
   // Add this as the last token by default, this will prevent infinite loops
   TOKENS.push(['0', /[\s\n\S]/])
-  let len = TOKENS.length
 
   while (STRING) {
-    for (let i = 0; i < len; i += 1) {
-      // Try to find a token. If not found, go to the next iteration of the loop
-      let map = TOKENS[i][1].exec(STRING)
-      if (!map || map.index !== 0) continue
-      let { type, value } = { type: TOKENS[i][0], value: map[0] }
-      let returnValue = CALLBACK ? CALLBACK({ type, value, map, index }) : { type, value }
-      if (returnValue) tokens.push(returnValue)
+    TOKENS.some(([type, regex]) => {
+      let map = regex.exec(STRING)
+      if (map && map.index === 0) {
+        // If not found, we are here
+        let value = map[0]
+        let returnValue = CALLBACK ? CALLBACK({ type, value, map, index }) : { type, value }
+        if (returnValue) tokens.push(returnValue) // TODO:
 
-      // Advance by slicing the string and push tokens to the list
-      STRING = STRING.slice(value.length)
-      index += value.length
-      break
-    }
+        // Advance by slicing the string and push tokens to the list
+        STRING = STRING.slice(value.length)
+        index += value.length
+        return true 
+      }
+    })
   }
   return tokens
-}
-
-export const getOccurences = (array, map, index) => {
-  let count = map[0].length
-  const whole = array.slice(index, count + index)
-  let occurences = [whole]
-  let cursor = 0
-  map.forEach((el, i) => {
-    if (i !== 0) {
-      if (el) {
-        count = el.length
-        const occ = whole.slice(cursor, count + cursor)
-        occurences.push(occ)
-        cursor = count + cursor
-      } else {
-        occurences.push(null)
-      }
-    }
-  })
-  occurences.map = map
-  occurences.index = index
-  occurences.count = map[0].length
-  return occurences
-}
-
-export const direct = (array, newState) => ({ type, value, map, index }) => {
-  const result = getOccurences(array, map, index)
-  const returnValue = type(result)
-  if (returnValue) {
-    if (Array.isArray(returnValue)) newState.add(returnValue[0], returnValue[1])
-    else newState.add('1', returnValue)
-  } else {
-    newState.noop(result)
-  }
-}
-
-export const replace = (self, tokens) => {
-  let newState = Foldmaker()
-  tokenize(self.string, tokens, direct(self.array, newState))
-  return newState
 }
 
 export const getTokensFromVisitors = (tokens, callback) => {
   if (tokens instanceof RegExp) tokens = [[callback, tokens]]
+  else tokens = tokens.map(([a,b]) => [b,a])
   // Add this as the last token by default, this will prevent infinite loops
-  tokens.push([() => null, /[\s\n\S]/])
+  tokens.push([() => undefined, /[\s\n\S]/])
   return tokens
+}
+
+export let visitor = (a, b, c) => ([a, b, c])
+
+export const getOccurrence = (array, map, index) => {
+  let count = map[0].length
+  return {
+    raw: array.slice(index, count + index),
+    index, count, map
+  }
+}
+
+export const replace = (self, tokens) => {
+  let state = Foldmaker()
+  let { string, array } = self
+  tokenize(string, tokens, ({ type, map,index }) => {
+    const occurrence = getOccurrence(array, map, index)
+    manipulate(state, type, occurrence)
+  })
+  return state
+}
+
+export const manipulate = (state, type, occurrence) => {
+    const result = type(occurrence)
+    if (result) {
+      if (Array.isArray(result)) state.concatenate(result[0], [result[1]])
+      else state.concatenate('1', [result])
+      state.modified = true
+    } else {
+      state.concatenate(occurrence.map[0], occurrence.raw)
+    }
 }
 
 const Foldmaker = tokens => new FoldmakerObject(tokens)
